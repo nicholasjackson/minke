b7265b4932da953e829117041344cfba1755d9d1
Nic Jackson <jackson.nic@gmail.com>
2016-06-24 10:09:40 -0700

    updated base dockerfile to use rvm

--- lib/minke/generators/shell_script.rb
+++ lib/minke/generators/shell_script.rb
@@ -2,12 +2,14 @@ module Minke
   module Generators
     SCRIPT = <<-EOF
     #!/bin/bash
-
     DOCKER_SOCK="/var/run/docker.sock:/var/run/docker.sock"
-    BUNDLE_COMMAND="set :bundle_flags, \"--path \#{shared_path}/bundles --quiet\" && bundle install -j3 && bundle update"
+    BUNDLE_COMMAND="bundle install -j3 && bundle update"
     ERROR="Please specify a command e.g. ./minke.sh rake app:test"
     COMMAND=""
     NEW_UUID=$(base64 /dev/urandom | tr -d '/+' | head -c 32 | tr '[:upper:]' '[:lower:]')
+    GEMSET=$(<.ruby-gemset)
+    GEMSETFOLDER="/usr/local/rvm/gems/ruby-2.3.0@${GEMSET}"
+
 
     if [ "$1" == '' ]; then
       echo $ERROR;
@@ -20,7 +22,7 @@ module Minke
     echo "Running command: ${COMMAND}"
 
     eval "docker network create minke_${NEW_UUID}"
-    eval "docker run --rm -it --net=minke_${NEW_UUID} -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR}/_build nicholasjackson/minke /bin/bash -c '${BUNDLE_COMMAND} && ${COMMAND}'"
+    eval "docker run --rm -it --net=minke_${NEW_UUID} -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -v ${DIR}/_build/vendor/gems:${GEMSETFOLDER} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR}/_build nicholasjackson/minke /bin/bash -c '${BUNDLE_COMMAND} && ${COMMAND}'"
     eval "docker network rm minke_${NEW_UUID}"
     EOF
 
@@ -29,6 +31,12 @@ module Minke
       File.chmod(0755, path)
     end
 
+    def create_rvm_files folder, application_name
+      File.write("#{folder}.ruby-gemset", application_name)
+      File.write("#{folder}.ruby-version", '2')
+    end
+
     module_function :write_bash_script
+    module_function :create_rvm_files
   end
 end
