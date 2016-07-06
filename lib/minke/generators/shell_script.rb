2e5f00b91a052b509c277593224e8c1df830aa4d
Nic Jackson <jackson.nic@gmail.com>
2016-07-06 11:56:10 +0100

    updated to remove shel_script to separate file

--- lib/minke/generators/shell_script.rb
+++ lib/minke/generators/shell_script.rb
@@ -1,32 +1,7 @@
 module Minke
   module Generators
-    SCRIPT = <<-EOF
-    #!/bin/bash
-    DOCKER_SOCK="/var/run/docker.sock:/var/run/docker.sock"
-    RVM_COMMAND="source /usr/local/rvm/scripts/rvm"
-    ERROR="Please specify a command e.g. ./minke.sh rake app:test"
-    COMMAND=""
-    NEW_UUID=$(base64 /dev/urandom | tr -d '/+' | head -c 32 | tr '[:upper:]' '[:lower:]')
-    GEMSET=$(<.ruby-gemset)
-    GEMSETFOLDER="/usr/local/rvm/gems/ruby-2.3.1@${GEMSET}"
-
-    if [ "$1" == '' ]; then
-      echo $ERROR;
-      exit 1;
-    fi
-
-    COMMAND=$*
-    DIR=$(dirname `pwd`)
-
-    echo "Running command: ${COMMAND}"
-
-    eval "docker network create minke_${NEW_UUID}"
-    eval "docker run --rm -it --net=minke_${NEW_UUID} -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -v ${DIR}/_build/vendor/gems:${GEMSETFOLDER} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR}/_build nicholasjackson/minke /bin/bash -c '${RVM_COMMAND} && ${COMMAND}'"
-    eval "docker network rm minke_${NEW_UUID}"
-    EOF
-
     def write_bash_script path
-      File.write(path, SCRIPT)
+      FileUtils.cp(File.expand_path('../../scripts/minke.sh', __FILE__), path)
       File.chmod(0755, path)
     end
 
---
+++ lib/minke/scripts/minke.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DOCKER_SOCK="/var/run/docker.sock:/var/run/docker.sock"
+RVM_COMMAND="source /usr/local/rvm/scripts/rvm"
+ERROR="Please specify a command e.g. ./minke.sh rake app:test"
+COMMAND=""
+NEW_UUID=$(base64 /dev/urandom | tr -d '/+' | head -c 32 | tr '[:upper:]' '[:lower:]')
+GEMSET=$(<.ruby-gemset)
+GEMSETFOLDER="/usr/local/rvm/gems/ruby-2.3.1@${GEMSET}"
+
+if [ "$1" == '' ]; then
+  echo $ERROR;
+  exit 1;
+fi
+
+COMMAND=$*
+DIR=$(dirname `pwd`)
+
+echo "Running command: ${COMMAND}"
+
+eval "docker network create minke_${NEW_UUID}"
+eval "docker run --rm -it --net=minke_${NEW_UUID} -v ${DOCKER_SOCK} -v ${DIR}:${DIR} -v ${DIR}/_build/vendor/gems:${GEMSETFOLDER} -e DOCKER_NETWORK=minke_${NEW_UUID} -w ${DIR}/_build nicholasjackson/minke /bin/bash -c '${RVM_COMMAND} && ${COMMAND}'"
+eval "docker network rm minke_${NEW_UUID}"
