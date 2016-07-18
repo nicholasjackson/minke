4b949ca027cfd3c1647840f737347632cf2a22e9
Nic Jackson <jackson.nic@gmail.com>
2016-07-18 20:16:03 +0100

    updated to stop webmock from interfering with code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ CodeClimate::TestReporter.start
 require 'webmock/rspec'
 require 'minke'
 
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
+
 RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
